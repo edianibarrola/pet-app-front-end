c773780fa0d5010fd590f7e9225f7a53459523ed
edianibarrola <edianibarrola@gmail.com>
2021-01-18 20:26:49 +0000

    update

--- src/js/views/calendar.js
+++ src/js/views/calendar.js
@@ -1,7 +1,64 @@
 import * as React from "react";
 import Paper from "@material-ui/core/Paper";
 import { ViewState } from "@devexpress/dx-react-scheduler";
-import { Scheduler, DayView, Appointments } from "@devexpress/dx-react-scheduler-material-ui";
+import { makeStyles } from "@material-ui/core/styles";
+import { fade } from "@material-ui/core/styles/colorManipulator";
+import { Scheduler, DayView, WeekView, MonthView, Appointments } from "@devexpress/dx-react-scheduler-material-ui";
+import appointments from "../../calendar-data/today-appointments";
+
+const useStyles = makeStyles(theme => ({
+	todayCell: {
+		backgroundColor: fade(theme.palette.primary.main, 0.1),
+		"&:hover": {
+			backgroundColor: fade(theme.palette.primary.main, 0.14)
+		},
+		"&:focus": {
+			backgroundColor: fade(theme.palette.primary.main, 0.16)
+		}
+	},
+	weekendCell: {
+		backgroundColor: fade(theme.palette.action.disabledBackground, 0.04),
+		"&:hover": {
+			backgroundColor: fade(theme.palette.action.disabledBackground, 0.04)
+		},
+		"&:focus": {
+			backgroundColor: fade(theme.palette.action.disabledBackground, 0.04)
+		}
+	},
+	today: {
+		backgroundColor: fade(theme.palette.primary.main, 0.16)
+	},
+	weekend: {
+		backgroundColor: fade(theme.palette.action.disabledBackground, 0.06)
+	}
+}));
+
+const TimeTableCell = props => {
+	const classes = useStyles();
+	const { startDate } = props;
+	const date = new Date(startDate);
+
+	if (date.getDate() === new Date().getDate()) {
+		return <WeekView.TimeTableCell {...props} className={classes.todayCell} />;
+	}
+	if (date.getDay() === 0 || date.getDay() === 6) {
+		return <WeekView.TimeTableCell {...props} className={classes.weekendCell} />;
+	}
+	return <WeekView.TimeTableCell {...props} />;
+};
+
+const DayScaleCell = props => {
+	const classes = useStyles();
+	const { startDate, today } = props;
+
+	if (today) {
+		return <WeekView.DayScaleCell {...props} className={classes.today} />;
+	}
+	if (startDate.getDay() === 0 || startDate.getDay() === 6) {
+		return <WeekView.DayScaleCell {...props} className={classes.weekend} />;
+	}
+	return <WeekView.DayScaleCell {...props} />;
+};
 
 const currentDate = "2018-11-01";
 const schedulerData = [
@@ -11,9 +68,15 @@ const schedulerData = [
 
 export const Calendar = () => (
 	<Paper>
-		<Scheduler data={schedulerData}>
+		<Scheduler data={appointments}>
 			<ViewState currentDate={currentDate} />
 			<DayView startDayHour={9} endDayHour={14} />
+			<WeekView
+				startDayHour={9}
+				endDayHour={14}
+				timeTableCellComponent={TimeTableCell}
+				dayScaleCellComponent={DayScaleCell}
+			/>
 			<Appointments />
 		</Scheduler>
 	</Paper>
