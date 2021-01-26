3f73dae65c8f53941eff1adae610ad4197716ca0
edianibarrola <edianibarrola@gmail.com>
2021-01-26 22:43:10 +0000

    edit pet working... finally -edi

--- src/js/component/petCard.js
+++ src/js/component/petCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { PetModal } from "./petModal";
 import Button from "react-bootstrap/Button";
+import { Context } from "../store/appContext";
 // const adoptable = {
 // 	Name: "Meeshka",
 // 	PetType: "Dog",
@@ -13,17 +14,12 @@ import Button from "react-bootstrap/Button";
 // 	Color: "Yellowish"
 // };
 
-export class PetCard extends React.Component {
-	constructor(props) {
-		super(props);
+export const PetCard = props => {
+	const { store, actions } = useContext(Context);
+	const [isOpen, setIsOpen] = useState(false);
+	const [toggleModal, setToggleModal] = useState(false);
+	const info = store.petList.find(element => element.id == props.pet_prop.id);
 
-		this.state = {
-			name: this.props.pet_prop.name,
-			pet_type: this.props.pet_prop.pet_type,
-			sex: this.props.pet_prop.sex,
-			color: this.props.pet_prop.color
-		};
-	}
 	// componentDidMount() {
 	// 	this.setState({
 	// 		//petInfo: jsonified results from api call,
@@ -35,75 +31,69 @@ export class PetCard extends React.Component {
 	// 	});
 	// }
 
-	render() {
-		return (
-			<Accordion defaultActiveKey="0">
-				<div className="p-4 m-4 pcGradientDarkGreen minw-20 pcLightGreenBorderSmall">
-					<Card style={{ width: "18rem" }}>
-						<Accordion.Toggle as={Card.Header} eventKey="0" className="pcGradientLightGreen">
-							<div className="d-flex justify-content-center ">
-								<Image src="https://picsum.photos/150" roundedCircle fluid />
+	return (
+		<Accordion defaultActiveKey="0">
+			<div className="p-4 m-4 pcGradientDarkGreen minw-20 pcLightGreenBorderSmall">
+				<Card style={{ width: "18rem" }}>
+					<Accordion.Toggle as={Card.Header} eventKey="0" className="pcGradientLightGreen">
+						<div className="d-flex justify-content-center ">
+							<Image src="https://picsum.photos/150" roundedCircle fluid />
+						</div>
+						<div className=" text-center">
+							<h2>{info.name}</h2>
+						</div>
+					</Accordion.Toggle>
+					<Accordion.Collapse eventKey="0">
+						<Card.Body>
+							<div className="container text-nowrap">
+								<li>
+									{" Pet Type "}
+									{info.pet_type}{" "}
+								</li>
+								<li>
+									{" sex "}
+									{info.sex}
+								</li>
+								<li>
+									{" color "}
+									{info.color}
+								</li>
 							</div>
-							<div className=" text-center">
-								<h2>{this.state.name}</h2>
-							</div>
-						</Accordion.Toggle>
-						<Accordion.Collapse eventKey="0">
-							<Card.Body>
-								<div className="container text-nowrap">
-									<li>
-										{" Pet Type "}
-										{this.state.pet_type}{" "}
-									</li>
-									<li>
-										{" sex "}
-										{this.state.sex}
-									</li>
-									<li>
-										{" color "}
-										{this.state.color}
-									</li>
-								</div>
 
-								<PetModal
-									show={this.state.isOpen}
-									onClose={this.toggleModal}
-									petValues={this.props.pet_prop}
-								/>
-							</Card.Body>
-						</Accordion.Collapse>
-					</Card>
-				</div>
-			</Accordion>
+							<PetModal show={isOpen} onClose={toggleModal} petValues={info} />
+						</Card.Body>
+					</Accordion.Collapse>
+				</Card>
+			</div>
+		</Accordion>
 
-			// <>
-			// 	<div className="card cardSmall m-4 p-4 col-3" style={{ width: "auto" }}>
-			// 		<div className="cardBGSmall">
-			// 			<div className="d-flex no-wrap card-bodySmall align-items-center">
-			// 				<img className="card-img-left m-4" src="https://via.placeholder.com/150" alt="Card image" />
-			// 				<h4 className="card-title">{this.state.val0}</h4>
-			// <div className="container">
-			// 	<li>
-			// 		{this.state.key1}
-			// 		{this.state.val1}{" "}
-			// 	</li>
-			// 	<li>
-			// 		{" "}
-			// 		{this.state.key2} {this.state.val2}
-			// 	</li>
-			// 	<li>
-			// 		{" "}
-			// 		{this.state.key3} {this.state.val3}
-			// 	</li>
-			// </div>
+		// <>
+		// 	<div className="card cardSmall m-4 p-4 col-3" style={{ width: "auto" }}>
+		// 		<div className="cardBGSmall">
+		// 			<div className="d-flex no-wrap card-bodySmall align-items-center">
+		// 				<img className="card-img-left m-4" src="https://via.placeholder.com/150" alt="Card image" />
+		// 				<h4 className="card-title">{this.state.val0}</h4>
+		// <div className="container">
+		// 	<li>
+		// 		{this.state.key1}
+		// 		{this.state.val1}{" "}
+		// 	</li>
+		// 	<li>
+		// 		{" "}
+		// 		{this.state.key2} {this.state.val2}
+		// 	</li>
+		// 	<li>
+		// 		{" "}
+		// 		{this.state.key3} {this.state.val3}
+		// 	</li>
+		// </div>
 
-			// 			</div>
-			// 		</div>
-			// 	</div>
-			// </>
-		);
-	}
-}
+		// 			</div>
+		// 		</div>
+		// 	</div>
+		// </>
+	);
+};
 
 PetCard.propTypes = {
 	pet_prop: PropTypes.object
