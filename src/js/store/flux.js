8209226fc98b418fd472f34cdf44176cff5080aa
George Mihov <gmihov001@gmail.com>
2021-01-27 15:26:50 +0000

    Fixed conditional in router

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -148,7 +148,6 @@ const getState = ({ getStore, getActions, setStore }) => {
 					})
 					.then(jsonifiedResponse => {
 						console.log(jsonifiedResponse);
-
 						fetch(getStore().url + "pet")
 							.then(function(response) {
 								if (!response.ok) {
