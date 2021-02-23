52c563bf4640b431bd50790907157f97ae8b10ae
edianibarrola <edianibarrola@gmail.com>
2021-02-23 16:34:23 +0000

    css tweaks -edian

--- src/js/views/map.js
+++ src/js/views/map.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import Iframe from "react-iframe";
 //npm i react-iframe
 export const Map = () => (
 	<div className="mt-5 pt-5">
-		<h1>map</h1>
+		<h1>Find services near you.</h1>
 		<Iframe
 			url="https://www.yelp.com/search?find_desc=vet&find_loc=miami"
 			width="100%"
@@ -16,34 +15,3 @@ export const Map = () => (
 		/>
 	</div>
 );
-=======
-// import React, { Component } from "react";
-// import GoogleMapReact from "google-map-react";
-// //npm install --save google-map-react
-// // const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-// class SimpleMap extends Component {
-// 	static defaultProps = {
-// 		center: {
-// 			lat: 59.95,
-// 			lng: 30.33
-// 		},
-// 		zoom: 11
-// 	};
-
-// 	render() {
-// 		return (
-// 			// Important! Always set the container height explicitly
-// 			<div style={{ height: "100vh", width: "100%" }}>
-// 				<GoogleMapReact
-// 					bootstrapURLKeys={{ key: "AIzaSyB2ot_ToPoOFGYON7yjbNLywYVdtHAYMiI" }}
-// 					// defaultCenter={this.props.center}
-// 					// defaultZoom={this.props.zoom}
-// 				/>
-// 			</div>
-// 		);
-// 	}
-// }
-
-// export default SimpleMap;
->>>>>>> cb010b1651ea27bc494aabcfbf465d8663d264df
